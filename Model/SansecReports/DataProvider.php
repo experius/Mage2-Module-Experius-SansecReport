6649fc5d8f4226b25f9e79704cb6a4cb40d55bc3
Floris Schreuder <floris@experius.nl>
2021-12-22 17:20:30 +0000

    [FEATURE][SOLUI-165] Fix for DI Compile in docblocks

--- Model/SansecReports/DataProvider.php
+++ Model/SansecReports/DataProvider.php
@@ -31,9 +31,9 @@ class DataProvider extends AbstractDataProvider
 
     /**
      * DataProvider constructor.
-     * @param $name
-     * @param $primaryFieldName
-     * @param $requestFieldName
+     * @param string $name
+     * @param string $primaryFieldName
+     * @param string $requestFieldName
      * @param CollectionFactory $collectionFactory
      * @param DataPersistorInterface $dataPersistor
      * @param array $meta
